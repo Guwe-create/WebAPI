a2545d5dc634a3186ba11c3ac7d578cad100c78a
Roy <whroy@email.neit.edu>
2023-01-12 14:23:01 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+//My stupid code bro
+var msg = "Bro Ryan is so stupid ngl.";
+
+var nut = 50;
+
+nut = nut * 2;
+
+msg = msg + " " + nut.toString();
+//The end of my trash
+
+//The main functions going on
+var express = require("express");
+var app = express();
+var local = 3000;
+//var path = require("path");
+
+//setup a route for static files
+app.use(express.static(__dirname+"/static"));
+
+//main route
+app.get("/", function(req,res){
+    //res.send(msg);
+    res.sendFile(__dirname+"/static/index.html");
+});
+
+app.get("/home", function(req,res){
+    res.send("This is the home page.");
+});
+
+app.listen(local, function(){
+    console.log("Running on localhost: " + local)
+});
+
